db18bf521a2564f419cd343bd9bba55455d99c17
sathishkumaravelu <sathishkumaravelu@gmail.com>
2023-12-24 00:13:59 +0530

    Hooks Git check

--- src/test/java/SanityBuild/LearningPOC/HooksTest.java
+++ src/test/java/SanityBuild/LearningPOC/HooksTest.java
@@ -5,6 +5,6 @@ public class HooksTest {
     public void test(){
         System.out.println("******************************");
         System.out.println("TESTING FOR HOOKS JENKINS");
-        System.out.println("*****************************");
+        System.out.println("*******************************");
     }
 }
